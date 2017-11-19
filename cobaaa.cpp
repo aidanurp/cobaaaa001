4886cddec814b00a762d69de1af72149bb8b9df8
aidanurp <14maret1998@gmail.com>
2017-11-19 21:50:47 +0700

    memasukkan program pilihan

--- cobaaa.cpp
+++ cobaaa.cpp
@@ -3,5 +3,19 @@
 using namespace std;
 
 int main(){
+	int lbr,gt,pjk,gk;
+	int gpkk,jk;
+	int panjang, lebar, luas ,pilihan, alas, tinggi;
+	int x;
+	int w,X,y;
+	double Tinggi, berat;
+	char yt;
+	atas:
 	
+	cout <<"1.Perhitungan berat badan\n";
+	cout <<"2.Perhitungan absolute\n";
+	cout <<"3.Persegi panjang\n";
+	cout <<"4.Perhitungan Gaji Karyawan\n";
+	cout <<"5.Penentuan Nilai\n";
+	cout <<"Masukan Pilihan =";cin >>pilihan;	
 }
